59c0c78861fc8765a1a688ee5e2674709912108b
rachadoobee <147326386+rachadoobee@users.noreply.github.com>
2024-01-17 15:45:03 +0000

    added sheff united academy photo + info, added match info, added headers + captions to each section

--- client/get_timeline_info.js
+++ client/get_timeline_info.js
@@ -33,3 +33,15 @@ document.getElementById("su21").addEventListener('click',function(event){
         console.error("Error:", error);
     });
 });
+
+document.getElementById("sua").addEventListener('click',function(event){
+    fetch("http://127.0.0.1:8090/club?".concat("sua"))
+    .then(response=>response.json())
+    .then(body=> {
+        document.getElementById("clubName").innerHTML=body[0];
+        document.getElementById("content").innerHTML=body[1];
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+});
