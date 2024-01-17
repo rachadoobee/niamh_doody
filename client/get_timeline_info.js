a94587811ddb9aef3b4a172abe1f395cbe0d9fe6
rachadoobee <147326386+rachadoobee@users.noreply.github.com>
2024-01-17 14:57:18 +0000

    get request added for timeline information

---
+++ client/get_timeline_info.js
@@ -0,0 +1,35 @@
+document.getElementById("du1s").addEventListener('click',function(event){
+    fetch("http://127.0.0.1:8090/club?".concat("du1s"))
+    .then(response=>response.json())
+    .then(body=> {
+        document.getElementById("clubName").innerHTML=body[0];
+        document.getElementById("content").innerHTML=body[1];
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+});
+
+document.getElementById("drb").addEventListener('click',function(event){
+    fetch("http://127.0.0.1:8090/club?".concat("drb"))
+    .then(response=>response.json())
+    .then(body=> {
+        document.getElementById("clubName").innerHTML=body[0];
+        document.getElementById("content").innerHTML=body[1];
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+});
+
+document.getElementById("su21").addEventListener('click',function(event){
+    fetch("http://127.0.0.1:8090/club?".concat("su21"))
+    .then(response=>response.json())
+    .then(body=> {
+        document.getElementById("clubName").innerHTML=body[0];
+        document.getElementById("content").innerHTML=body[1];
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+});
--- server.js
+++ server.js
@@ -2,4 +2,18 @@ const express = require('express');
 const app = express();
 app.use(express.static('client'));
 
+var clubs = require("./data/club_info.json");
+
+app.get("/club",function(req, resp){
+    clubName = Object.keys(req.query)[0]
+    let info = []
+    for (const club of clubs){
+        if (club["tags"].includes(clubName)){
+            info.push(club["club"])
+            info.push(club["text"])
+        }
+    }
+    resp.send(info)
+});
+
 app.listen(8090);
